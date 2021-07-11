c6548a805cbd1046482eb6af1318af50c5872f2b
CHLIN <linch.lin88@gmail.com>
2021-07-11 19:16:18 +0800

    cicd

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ async def slow_route():
     #         data = await resp.text()
     #         print(data)
     #         return data
-    return "hello"
+    return "hello123"
             # do something with data
 
 
